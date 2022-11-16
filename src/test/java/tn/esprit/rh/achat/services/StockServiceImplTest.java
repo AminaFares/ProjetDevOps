9732f2686b5bf16695c2de67cf518f4a3b34ad72
farah5msehli <79632955+farah5msehli@users.noreply.github.com>
2022-11-16 21:21:15 +0100

    Update StockServiceImplTest.java

--- src/test/java/tn/esprit/rh/achat/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/StockServiceImplTest.java
@@ -1,66 +1,64 @@
 /*package tn.esprit.rh.achat.services;
 
-
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-@TestMethodOrder(OrderAnnotation.class)
-class StockServiceImplTest {
+@ExtendWith(MockitoExtension.class)
+class OperateurServiceImplMock {
 
-    @Autowired
-    IStockService St;
+	    @Mock
+	    OperateurRepository OperateurRepository;
+	    
+	    @InjectMocks
+	    OperateurServiceImpl MySevice;
 
-    @Test
-    @Order(1)
-    void retrieveAllStocks() {
-        List<Stock> listUsers = St.retrieveAllStocks();
-        assertEquals(0, listUsers.size());
-    }
+	    Operateur Operateur = new Operateur("Op1", "surname1", "pwd1");
+	    List<Operateur> listOperateurs = new ArrayList<Operateur>() {
+	        {
+	            add(new Operateur("Op2", "surname2", "pwd2"));
+	            add(new Operateur("Op3", "surname3", "pwd3"));
+	        }
+	    };
 
-    @Test
-    void addStock() {
-        Stock s1 = new Stock();
-        s1.setLibelleStock("stock test");
-        s1.setQte(10);
-        s1.setQteMin(100);
-        Stock savedStock1= St.addStock(s1);
-        assertEquals(s1.getLibelleStock(), savedStock1.getLibelleStock());
-    }
-
-    @Test
-    void deleteStock() {
-        St.deleteStock(0L);
-    }
-
-    @Test
-    void updateStock() {
-        Stock s1= St.retrieveStock(0L);
-        s1.setQte(100);
-        Stock updatedStock1= St.updateStock(s1);
-        assertEquals(s1.getQte(), updatedStock1.getQte());
-    }
-
-    @Test
-    void retrieveStock() {
-        St.retrieveStock(0L);
-    }
-
-    @Test
-    void retrieveStatusStock() {
-        St.retrieveStatusStock();
-    }
-
-
-}*/
-
-
-
+	    @Test
+	    void retrieveOperateurTestM() {
+	        Mockito.when(OperateurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(Operateur));
+	        Operateur Operateur1 = MySevice.retrieveOperateur(0L);
+	        Assertions.assertNotNull(Operateur1);
+	    }
+	    @Test
+	    void addOperateurTestM() {
+	        Operateur Operateur = new Operateur("Op3", "surname4", "pwd9");
+	        Mockito.when(OperateurRepository.save(ArgumentMatchers.any(Operateur.class))).thenReturn(Operateur);
+	        Operateur created = MySevice.addOperateur(Operateur);
+	        Assertions.assertSame(created,Operateur);
+	        Mockito.verify(OperateurRepository).save(Operateur);
+	    }
+	    @Test
+	    void updateOperateurTestM() {
+	    
+	       Operateur Operateur2 = new Operateur("Op9", "surname1", "pwd1");
+	       Operateur2.setIdOperateur(25L);
+	       Mockito.lenient().when(OperateurRepository.findById(Operateur2.getIdOperateur())).thenReturn(Optional.of(Operateur2));
+      when(OperateurRepository.save(Operateur2)).thenReturn(Operateur2);
+	        MySevice */
